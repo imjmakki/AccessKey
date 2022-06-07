2f1997a6c16e46e640b4c2a51fcd392ee74b2643
Mohamad J. Makki <malanbaki@transforat.com>
2022-06-07 13:16:16 +0300

    Start working on access key project

---
+++ src/main/java/app/accesskey/v1/AccessKeyApplication.java
@@ -0,0 +1,13 @@
+package app.accesskey.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/accesskey/v1/AccessKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.accesskey.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
